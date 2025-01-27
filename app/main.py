0ee80919e74c2b9bb3423cc5dc0d418590e6c3af
Taito1608 <taitoyusa@icloud.com>
2025-01-27 16:18:39 +0900

    index.htmlファイル追加

--- app/main.py
+++ app/main.py
@@ -1,13 +1,13 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
-from typing import Union
-from typing import Annotated    #クエリパラメータの最小値と最大値を設定するため（大量のデータを一度に取得しようとするとサービスの遅延に繋がるため）
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 from .database.setting import SessionLocal
 from .database.table.models import Todo
 
-from fastapi import FastAPI, Query
-
 app = FastAPI()
+app.mount(path="/app/static", app=StaticFiles(directory="app/static"), name="static")
+templates = Jinja2Templates(directory="app/templates")
 
 @app.get("/todo/{todo_id}")
 def read_todo(todo_id: str):
@@ -15,5 +15,9 @@ def read_todo(todo_id: str):
     todo = db.query(Todo).filter(Todo.id == todo_id).first()
     print(f"読み込んだレコード: {todo.id}, {todo.box}, {todo.date}, {todo.done}")
     return {f"読み込んだレコード: {todo.id}, {todo.box}, {todo.date}, {todo.done}"}
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request, "message": "Hello, FastAPI!"})
 
 
